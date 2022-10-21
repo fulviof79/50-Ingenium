47b8c6f653d5d666a7ba5916251cea6a9e9ecb7d
fulviof79 <40266549+fulviof79@users.noreply.github.com>
2022-10-21 11:05:51 +0200

    cosmetica assi e rappresentazione zero con pallino rosso

--- Python/newton_basic.py
+++ Python/newton_basic.py
@@ -1,34 +1,41 @@
 #%%
 
+from matplotlib.lines import lineStyles
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-
+#Define function f
 def f(x):
     return 4*x**4+2*x-1
 
+#Define derivative of the function f
 def df(x):
     return 16*x**3+2
 
-
-xvalues= np.linspace(-1,1,100)
-yvalues = f(xvalues)
-
 #Plot
-#Plot parameter, 
+
+#Plot parameters, 
 plot_xmin=-1
 plot_xmax=1
 total_points=1000
-
+#Calculate total_points sample of function f (for the plot)
 xvalues= np.linspace(plot_xmin,plot_xmax,total_points)
 yvalues = f(xvalues)
 
-plt.axhline(0, color='r')
-plt.axvline(0,color='r')
-plt.plot(xvalues,yvalues)
+#Prepare and draw figure
+fig = plt.figure() 
+ax = fig.add_subplot(1,1,1)
+#Dashed axes
+ax.axhline(0, color='k', linestyle='--')
+ax.axvline(0,color='k', linestyle='--')
+ax.set_ylim(min(yvalues)-2,max(yvalues)+1)
+#Plot samples of function f
+ax.plot(xvalues,yvalues)
 
-plt.show()
+
+
+
 #define precision
 delta=1e-10
 #range of initial guesses
@@ -39,23 +46,36 @@ amount=100
 
 
 # Initial guess
-initial_guess=-0.55
+initial_guess=-1
 
-
+ERROR=False
 #Main algorithm
 guess=initial_guess
 for n in range(max_iterations):
     if df(guess) !=0:
         next_guess = guess - f(guess)/df(guess)
     else:
+        print("Error : derivative is 0 at x={}".format(guess))
+        ERROR=True
         break
-        #TODO Catch error
+        
     if abs(next_guess-guess)< delta:
         break
     guess=next_guess
 
 #Print result
-print("Starting with initial guess at {}".format(initial_guess))
-print("root at {}".format(guess))
+if not(ERROR):
+    print("Starting with initial guess at {}".format(initial_guess))
+    print("root at {}".format(guess))
+
+ax.plot(guess, f(guess), 'or')
+
+ax.annotate("{:.2f}".format(guess), # this is the text
+                 (guess,f(guess)), # these are the coordinates to position the label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,-20), # distance from text to points (x,y)
+                 ha='center')
+
+
 
 # %%
