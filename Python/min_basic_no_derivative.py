02c9b1e59a1406ee58978a8572f76a65f82e2f20
fulviof79 <40266549+fulviof79@users.noreply.github.com>
2023-03-08 15:27:29 +0100

    minor

--- Python/min_basic_no_derivative.py
+++ Python/min_basic_no_derivative.py
@@ -6,7 +6,7 @@ import numpy as np
 
 #Function to analyze
 def f(x):
-    return (x**2-2)/np.exp(x)-3
+    return (x**3+5*x**2+3*x-6)
 
 #Numerical approximation of derivative
 def ndf(x, derivative_precision=0.01):
@@ -15,8 +15,8 @@ def ndf(x, derivative_precision=0.01):
 
 #Plot
 #Plot parameter, 
-plot_xmin=-1
-plot_xmax=3
+plot_xmin=-5
+plot_xmax=5
 total_points=1000
 
 xvalues= np.linspace(plot_xmin,plot_xmax,total_points)
@@ -25,7 +25,7 @@ yvalues = f(xvalues)
 plt.axhline(0, color='r')
 plt.axvline(0,color='r')
 plt.plot(xvalues,yvalues)
-
+plt.ylim(-10, 10)
 plt.show()
 
 #define Parameters
@@ -34,7 +34,7 @@ delta=1e-10
 #Max number of iterations
 max_iterations=10000
 #Initial guess
-initial_guess=2
+initial_guess=-4
 #Learning rate
 learning_rate=0.01
 
@@ -42,7 +42,7 @@ learning_rate=0.01
 guess=initial_guess
 for n in range(max_iterations):
     #Follow descent direction
-    next_guess = guess - learning_rate*( ndf(guess))
+    next_guess = guess + learning_rate*( ndf(guess))
     
     #Stop id desired precision in reached
     if abs(f(next_guess)-f(guess))  < delta:
@@ -52,7 +52,7 @@ for n in range(max_iterations):
    
 #Print results
 print("Starting the search at {}".format(initial_guess))
-print("Found a local minimum at {}".format(guess) )
+print("Found a local maximum at {}".format(guess) )
 
 
 # %%
