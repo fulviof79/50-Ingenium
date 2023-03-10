373ad4e4e2c594a472e36fa762755e8ad102bfc6
fulviof79 <40266549+fulviof79@users.noreply.github.com>
2023-03-10 20:53:52 +0100

    grafici

--- Python/volume_cono.py
+++ Python/volume_cono.py
@@ -2,11 +2,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #%%
+def disc_radius(radius,height,x):
+    '''
+    Ritorna il raggio della sezione (disco) a distanza x dal vertice di un cono di raggio=radius e altezza=height
+    '''
+    return (radius / height * x)   
+
 def disc_area(radius,height,x):
     '''
     Ritorna l'area della sezione (disco) a distanza x dal vertice di un cono ri raggio=radius e altezza=height
     '''
-    return (np.pi * radius**2 / height **2 * x**2)
+    return np.pi *pow(disc_radius(radius,height,x),2)
 
 def volume_out(radius,height,slices):
     '''
@@ -43,11 +49,22 @@ print("Raggio={} Altezza={} \n\n Volume con dischi esterni = {}  \n Volume con d
 
 # %%
 # Plot dell'andamento dell area dei dischi e quindi del volume
-slices=50
+slices=10
+x=np.linspace(start=height,stop=0,num=slices)
 x_out=np.linspace(start=height,stop=0,num=slices,endpoint=False)
+dx_out=x_out[-1]
+y_out=x_out-dx_out
 x_in=np.linspace(start=0,stop=height,num=slices,endpoint=False)
 
-plt.bar(x_out,disc_area(radius,height,x_out),width=x[-1], color='red')
-#plt.bar(x_in,disc_area(radius,height,x_in),width=x_in[1], color='blue')
+plt.bar(y_out,disc_area(radius,height,x_out),width=dx_out, color='red',alpha=0.2,align='edge')
+plt.bar(x_in,disc_area(radius,height,x_in),width=x_in[1], color='blue',alpha=0.2,align='edge')
+plt.plot(x,disc_area(radius,height,x), color='black')
+plt.scatter(x_out,disc_area(radius,height,x_out))
+
+
+# %%
+
+# %%
+
 
 # %%
